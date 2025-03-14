5ed2ef79a848de573dc35234a97c2e08abc9f1c0
Mohamed Ezz Elarab <e.mohamed@leadergroup.com>
2025-03-14 20:55:07 +0200

    Add Excel Importer Method

---
+++ ExcelToC#Importer/Models/ExcelColumnAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHandler
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExcelColumnAttribute : Attribute
+    {
+        public string ColumnName { get; }
+
+        public ExcelColumnAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+    }
+}
--- ExcelToC#Importer/Models/ExcelImporter.cs
+++ ExcelToC#Importer/Models/ExcelImporter.cs
@@ -1,56 +1,149 @@
-﻿//using OfficeOpenXml;
+﻿using ClosedXML.Excel;
+using DataHandler;
+using System.Reflection;
 
-//namespace ExcelImporterMethod
-//{
-//    public static class ExcelImporter
-//    {
-//        public static List<T> ReadExcelData<T>(string filePath) where T : new()
-//        {
-//            List<T> dataList = new List<T>();
+namespace ExcelImporterMethod
+{
+    public static class ExcelImporter
+    {
+        public static List<T> ReadExcelDataUsingPropertyNames<T>(string filePath) where T : new()
+        {
+            List<T> dataList = new List<T>();
 
-//            using (var package = new ExcelPackage(new FileInfo(filePath)))
-//            {
-//                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-//                var worksheet = package.Workbook.Worksheets[0];
-//                int rowCount = worksheet.Dimension.Rows;
-//                int colCount = worksheet.Dimension.Columns;
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var worksheet = workbook.Worksheet(1);
+                var rows = worksheet.RowsUsed();
 
-//                var headers = new List<string>();
-//                for (int col = 1; col <= colCount; col++)
-//                {
-//                    headers.Add(worksheet.Cells[1, col].Text);
-//                }
+                var headers = new List<string>();
+                var firstRow = rows.First();
+                foreach (var cell in firstRow.Cells())
+                {
+                    headers.Add(cell.Value.ToString());
+                }
 
-//                var properties = typeof(T).GetProperties();
+                var properties = typeof(T).GetProperties();
 
-//                for (int row = 2; row <= rowCount; row++)
-//                {
-//                    T data = new T();
+                foreach (var row in rows.Skip(1))
+                {
+                    T data = new T();
 
-//                    for (int col = 1; col <= colCount; col++)
-//                    {
-//                        string header = headers[col - 1];
-//                        string cellValue = worksheet.Cells[row, col].Text;
+                    for (int col = 1; col <= headers.Count; col++)
+                    {
+                        string header = headers[col - 1];
+                        string cellValue = row.Cell(col).Value.ToString();
 
-//                        var property = properties.FirstOrDefault(p => p.Name.Equals(header, StringComparison.OrdinalIgnoreCase));
+                        var property = properties.FirstOrDefault(p => p.Name.Equals(header, StringComparison.OrdinalIgnoreCase));
 
-//                        if (property is not null && !string.IsNullOrEmpty(cellValue))
-//                        {
-//                            if (property.PropertyType == typeof(int))
-//                            {
-//                                property.SetValue(data, int.Parse(cellValue));
-//                            }
-//                            else
-//                            {
-//                                property.SetValue(data, cellValue);
-//                            }
-//                        }
-//                    }
-//                    dataList.Add(data);
-//                }
-//            }
+                        if (property != null && !string.IsNullOrEmpty(cellValue))
+                        {
+                            if (property.PropertyType == typeof(int))
+                            {
+                                property.SetValue(data, int.Parse(cellValue));
+                            }
+                            else if (property.PropertyType == typeof(double))
+                            {
+                                property.SetValue(data, double.Parse(cellValue));
+                            }
+                            else if (property.PropertyType == typeof(DateTime))
+                            {
+                                property.SetValue(data, DateTime.Parse(cellValue));
+                            }
+                            else
+                            {
+                                property.SetValue(data, cellValue);
+                            }
+                        }
+                    }
 
-//            return dataList;
-//        }
-//    }
-//}
+                    dataList.Add(data);
+                }
+            }
+
+            return dataList;
+        }
+
+        public static List<T> ReadExcelDataUsingAttributes<T>(string filePath) where T : new()
+        {
+            List<T> dataList = new List<T>();
+
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var worksheet = workbook.Worksheet(1);
+                var rows = worksheet.RowsUsed();
+
+                var headers = new List<string>();
+                var firstRow = rows.First();
+                foreach (var cell in firstRow.Cells())
+                {
+                    headers.Add(cell.Value.ToString().Trim());
+                }
+
+                var properties = typeof(T).GetProperties();
+                var columnMappings = new Dictionary<string, PropertyInfo>(StringComparer.OrdinalIgnoreCase);
+
+                foreach (var property in properties)
+                {
+                    var attribute = property.GetCustomAttribute<ExcelColumnAttribute>();
+                    string columnName = attribute != null ? attribute.ColumnName : property.Name;
+
+                    columnMappings[columnName.Trim()] = property;
+                }
+
+                foreach (var row in rows.Skip(1))
+                {
+                    T data = new T();
+
+                    for (int col = 1; col <= headers.Count; col++)
+                    {
+                        string header = headers[col - 1];
+                        var cell = row.Cell(col);
+
+                        if (cell.IsEmpty() || cell?.Value is null)
+                        {
+                            continue;
+                        }
+
+                        string cellValue = cell.Value.ToString().Trim();
+
+                        if (columnMappings.TryGetValue(header, out var property))
+                        {
+                            if (!string.IsNullOrEmpty(cellValue))
+                            {
+                                try
+                                {
+                                    if (property.PropertyType == typeof(int))
+                                    {
+                                        property.SetValue(data, int.Parse(cellValue));
+                                    }
+                                    else if (property.PropertyType == typeof(double))
+                                    {
+                                        property.SetValue(data, double.Parse(cellValue));
+                                    }
+                                    else if (property.PropertyType == typeof(DateTime))
+                                    {
+                                        property.SetValue(data, DateTime.Parse(cellValue));
+                                    }
+                                    else
+                                    {
+                                        property.SetValue(data, cellValue);
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Error converting value '{cellValue}' for property '{property.Name}': {ex.Message}");
+                                }
+                            }
+                        }
+                    }
+
+                    dataList.Add(data);
+                }
+            }
+
+            return dataList;
+        }
+    }
+}
+
+
